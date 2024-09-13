18646a01e2d32671acb2a529bd4f709893086b6f
rwpotter8 <rwpotter08@gmail.com>
2024-09-13 13:30:08 -0400

    CRUD

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Lets design!'},
-    {'id':3, 'name':'Lets develop!'},
-]
-
 def home(request):
-    context = {'rooms': room}    
-    return render(request, 'base/home.html', context    )
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}    
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
