d5b094745b4331a397740fae6a2853001a65225d
rwpotter8 <rwpotter08@gmail.com>
2024-09-13 13:30:08 -0400

    Database & Admin Panel

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -9,8 +9,13 @@ rooms = [
 ]
 
 def home(request):
-    context = {'rooms': room}    
-    return render(request, 'base/home.html', context    )
+    context = {'rooms': rooms}    
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
